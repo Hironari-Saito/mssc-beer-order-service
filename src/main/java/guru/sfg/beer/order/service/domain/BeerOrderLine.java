739d413c9de83b23cd2060bc552c9fa959863df2
Hironari-Saito <hiroshi31100@gmail.com>
2020-12-06 23:02:30 +0900

    update snapshop - bom

--- src/main/java/guru/sfg/beer/order/service/domain/BeerOrderLine.java
+++ src/main/java/guru/sfg/beer/order/service/domain/BeerOrderLine.java
@@ -16,10 +16,7 @@
  */
 package guru.sfg.beer.order.service.domain;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
@@ -34,14 +31,14 @@ import java.util.UUID;
 @NoArgsConstructor
 @Entity
 public class BeerOrderLine extends BaseEntity {
-
     @Builder
     public BeerOrderLine(UUID id, Long version, Timestamp createdDate, Timestamp lastModifiedDate,
-                         BeerOrder beerOrder, UUID beerId, Integer orderQuantity,
+                         BeerOrder beerOrder, UUID beerId, String upc, Integer orderQuantity,
                          Integer quantityAllocated) {
         super(id, version, createdDate, lastModifiedDate);
         this.beerOrder = beerOrder;
         this.beerId = beerId;
+        this.upc = upc;
         this.orderQuantity = orderQuantity;
         this.quantityAllocated = quantityAllocated;
     }
---
+++ src/main/java/guru/sfg/beer/order/service/services/beer/BeerServiceImpl.java
@@ -0,0 +1,54 @@
+package guru.sfg.beer.order.service.services.beer;
+
+import guru.sfg.beer.order.service.web.model.BeerDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Objects;
+import java.util.Optional;
+import java.util.UUID;
+
+@Slf4j
+@ConfigurationProperties(prefix = "sfg.brewery", ignoreUnknownFields = false)
+@Component
+public class BeerServiceImpl implements BeerService {
+
+    private final String BEER_PATH_V1 = "/api/v1/beer/";
+    private final String BEER_UPC_PATH_V1 = "/api/v1/beerUpc/";
+    private final RestTemplate restTemplate;
+
+    private String beerServiceHost;
+
+    public BeerServiceImpl(RestTemplateBuilder restTemplateBuilder) {
+        this.restTemplate = restTemplateBuilder.build();
+    }
+
+    public void setBeerServiceHost(String beerServiceHost) {
+        this.beerServiceHost = beerServiceHost;
+    }
+
+    @Override
+    public Optional<BeerDto> getBeerById(UUID uuid) {
+        return Optional.of(
+                restTemplate.getForObject(
+                        beerServiceHost + BEER_PATH_V1 + uuid.toString() , BeerDto.class
+                )
+        );
+    }
+
+    @Override
+    public Optional<BeerDto> getBeerByUpc(String upc) {
+        return Optional.of(
+                restTemplate.getForObject(
+                        beerServiceHost + BEER_UPC_PATH_V1 + upc , BeerDto.class
+                )
+        );
+    }
+
+}
